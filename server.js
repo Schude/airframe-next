1c614b9b0d866e73b16cdaea7238163030255c62
Serki <nemli.serkan2@gmail.com>
2022-04-26 17:25:39 +0300

    test
    test

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || (dev ? 4110 : 80);
-const app = next({ dev });
+const app = express();
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-    const server = express();
+    // const server = express();
 
-    server.use(compression());
+    app.use(compression());
 
-    server.get('*', (req, res) => {
+    app.get('*', (req, res) => {
         const parsedUrl = parse(req.url, true);
 
         handle(req, res, parsedUrl);
